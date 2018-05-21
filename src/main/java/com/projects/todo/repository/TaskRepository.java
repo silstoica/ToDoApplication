3569fdf70255ac0dcedaf79cfe3a78ca1f241414
silstoica <silvia.stoica@live.com>
2018-05-21 09:48:11 +0300

    Created repository layer

---
+++ src/main/java/com/projects/todo/repository/TaskRepository.java
@@ -0,0 +1,12 @@
+package com.projects.todo.repository;
+
+import com.projects.todo.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
---
+++ src/main/java/com/projects/todo/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.projects.todo.repository;
+
+import com.projects.todo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
